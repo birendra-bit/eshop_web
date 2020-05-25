1f83fc9f3bd9375ea4097d00e5e0447cd425a069
Birendra Bhujel <birendra.bhujel@cybergindia.local>
2020-05-25 12:58:08 +0530

    angular.json file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  url:'https://angular-eshoping-web-demo-2020.herokuapp.com'
+  production: false,
+  url:'http://localhost:5000'
 };
 
 /*
